1a06a02dbf1984cb3fd2b82b2d2bc11236d12954
YuHyun <puh0128@gmail.com>
2023-02-08 21:57:27 +0900

    feat: header 컴포넌트 구현

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,69 @@
+import { type ReactElement } from "react";
+import { NavLink, Link } from "react-router-dom";
+
+const DUMMY_USER = {
+  email: "yy@yy",
+  username: "yyy1",
+  bio: null,
+  image: "https://api.realworld.io/images/smiley-cyrus.jpeg",
+  token: "",
+};
+
+function Header(): ReactElement {
+  return (
+    <nav className="navbar navbar-light">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          conduit
+        </Link>
+        <ul className="nav navbar-nav pull-xs-right">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/">
+              Home
+            </NavLink>
+          </li>
+          {DUMMY_USER === null ? (
+            <>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/login">
+                  Sign in
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/register">
+                  Sign up
+                </NavLink>
+              </li>
+            </>
+          ) : (
+            <>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/editor">
+                  <i className="ion-compose" />
+                  &nbsp;New Article
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/settings">
+                  <i className="ion-gear-a" />
+                  &nbsp;Settings
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link"
+                  to={`/profile/${DUMMY_USER.username}`}
+                >
+                  <img className="user-pic" src={DUMMY_USER.image} alt="" />
+                  {DUMMY_USER.username}
+                </NavLink>
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
