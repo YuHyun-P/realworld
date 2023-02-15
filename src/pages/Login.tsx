3ba199b08849031ee0227374d110d4a70803e1b8
YuHyun <puh0128@gmail.com>
2023-02-15 18:52:34 +0900

    feat: 로그인 페이지 마크업

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,33 @@
-import { type ReactElement } from "react";
+import { type ReactElement, type FormEventHandler } from "react";
+import { Link } from "react-router-dom";
+import LoginForm from "../components/pages/login/LoginForm";
+import ErrorMessage from "../components/common/ErrorMessage";
 
 function Login(): ReactElement {
-  return <div>Login</div>;
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    const {
+      email: { value: email },
+      password: { value: password },
+    } = e.currentTarget;
+    console.log(email, password);
+  };
+
+  return (
+    <div className="auth-page">
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 col-xs-12">
+            <h1 className="text-xs-center">Sign in</h1>
+            <p className="text-xs-center">
+              <Link to="/register">Need an account?</Link>
+            </p>
+            <ErrorMessage message="error" />
+            <LoginForm onSubmit={handleSubmit} disabled={false} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
