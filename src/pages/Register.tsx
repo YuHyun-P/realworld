e7c9b5e527fdd7db06756866564860b80c9538cf
YuHyun <puh0128@gmail.com>
2023-02-15 22:17:26 +0900

    refactor: 로그인/회원가입 페이지 레이아웃 컴포넌트로 분리

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,6 @@
 import { type FormEventHandler, type ReactElement } from "react";
 import { Link } from "react-router-dom";
+import AuthPageLayout from "~/components/layout/AuthPageLayout";
 import RegisterForm from "~/components/pages/register/RegisterForm";
 import ErrorMessage from "../components/common/ErrorMessage";
 
@@ -14,21 +15,14 @@ function Register(): ReactElement {
   };
 
   return (
-    <div className="auth-page">
-      <div className="container page">
-        <div className="row">
-          <div className="col-md-6 offset-md-3 col-xs-12">
-            <h1 className="text-xs-center">Sign up</h1>
-            <p className="text-xs-center">
-              <Link to="/login">Have an account?</Link>
-            </p>
-
-            <ErrorMessage />
-            <RegisterForm onSubmit={handleSubmit} disabled={false} />
-          </div>
-        </div>
-      </div>
-    </div>
+    <AuthPageLayout>
+      <h1 className="text-xs-center">Sign up</h1>
+      <p className="text-xs-center">
+        <Link to="/login">Have an account?</Link>
+      </p>
+      <ErrorMessage />
+      <RegisterForm onSubmit={handleSubmit} disabled={false} />
+    </AuthPageLayout>
   );
 }
 
