f7adcdf17a8abc9c151d14dc446988e40ab1087d
YuHyun <puh0128@gmail.com>
2023-02-17 00:39:36 +0900

    feat: 홈 페이지 더미 데이터 추가

---
+++ src/test_data/tags.ts
@@ -0,0 +1,16 @@
+import { type Tag } from "~/types";
+
+const tags: Tag[] = [
+  "implementations",
+  "welcome",
+  "introduction",
+  "codebaseShow",
+  "ipsum",
+  "qui",
+  "et",
+  "quia",
+  "cupiditate",
+  "deserunt",
+];
+
+export default tags;
