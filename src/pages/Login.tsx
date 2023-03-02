5780dd9db2e1991b68d37ad9e9b08a06022cc5ae
YuHyun <puh0128@gmail.com>
2023-03-02 12:03:46 +0900

    feat: 로그인 API 연결

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,16 +1,55 @@
-import { type ReactElement, type FormEventHandler } from "react";
-import { Link } from "react-router-dom";
+import { type ReactElement, type FormEventHandler, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import AuthPageLayout from "~/components/layout/AuthPageLayout";
-import LoginForm from "../components/pages/login/LoginForm";
+import { useSetRecoilState } from "recoil";
+import userAtom from "~/recoil/atoms/userState";
+import userApi, { type LoginUserRequest } from "~/api/services/user";
+import { isAxiosError } from "axios";
+import { type ErrorResponse } from "~/types";
+import storage from "~/utils/storage";
+import { formatErrorResponse } from "../utils/formatter";
 import ErrorMessage from "../components/common/ErrorMessage";
+import LoginForm from "../components/pages/login/LoginForm";
 
 function Login(): ReactElement {
-  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+  const [error, setError] = useState<string[]>();
+  const [isLoading, setIsLoading] = useState(false);
+  const setUser = useSetRecoilState(userAtom);
+  const navigate = useNavigate();
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+    const payload = e.currentTarget
+      .elements as typeof e.currentTarget.elements & {
+      email: { value: string };
+      password: { value: string };
+    };
+
     const {
       email: { value: email },
       password: { value: password },
-    } = e.currentTarget;
-    console.log(email, password);
+    } = payload;
+
+    await fetchLogin({ email, password });
+  };
+
+  const fetchLogin = async (payload: LoginUserRequest): Promise<void> => {
+    setIsLoading(true);
+    try {
+      const {
+        data: { user },
+      } = await userApi.login(payload);
+
+      setUser(user);
+      storage("local").setItem("token", user.token);
+      navigate("/");
+    } catch (err) {
+      if (isAxiosError<ErrorResponse>(err)) {
+        setError(formatErrorResponse(err));
+      } else {
+        setError(["something went wrong"]);
+      }
+    }
+    setIsLoading(false);
   };
 
   return (
@@ -19,8 +58,8 @@ function Login(): ReactElement {
       <p className="text-xs-center">
         <Link to="/register">Need an account?</Link>
       </p>
-      <ErrorMessage />
-      <LoginForm onSubmit={handleSubmit} disabled={false} />
+      <ErrorMessage error={error} />
+      <LoginForm onSubmit={handleSubmit} disabled={isLoading} />
     </AuthPageLayout>
   );
 }
