164c52e18d0365fc4873b76b98ac782ba90eab1c
YuHyun <puh0128@gmail.com>
2023-03-07 17:14:07 +0900

    feat: 회원 정보 갱신 API mocking

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,16 +1,17 @@
 import { rest } from "msw";
 import { BASE_URL } from "~/api/base";
-import {
-  type LoginUserRequest,
-  type RegisterUserRequest,
-} from "~/api/services/user";
 import MOCK_USER from "~/test_data/user";
 
 // https://github.com/mswjs/msw/issues/397#issuecomment-751230924
 export const baseUrl = (path: string): string => BASE_URL + path;
 
+const UnauthorizedErrorBody = {
+  status: "error",
+  message: "missing authorization credentials",
+};
+
 const handlers = [
-  rest.post<RegisterUserRequest>(baseUrl("/users"), async (req, res, ctx) => {
+  rest.post(baseUrl("/users"), async (req, res, ctx) => {
     const {
       user: { username, email, password },
     } = await req.json();
@@ -39,33 +40,60 @@ const handlers = [
       })
     );
   }),
-  rest.post<LoginUserRequest>(
-    baseUrl("/users/login"),
-    async (req, res, ctx) => {
-      const {
-        user: { email, password },
-      } = await req.json();
+  rest.post(baseUrl("/users/login"), async (req, res, ctx) => {
+    const {
+      user: { email, password },
+    } = await req.json();
 
-      const errorResponse: Record<string, string> = {};
-      if (email === "") {
-        errorResponse.email = "can't be blank";
-        return await res(ctx.status(422), ctx.json({ errors: errorResponse }));
-      }
-      if (password === "") {
-        errorResponse.password = "can't be blank";
-        return await res(ctx.status(422), ctx.json({ errors: errorResponse }));
-      }
-
-      return await res(
-        ctx.json({
-          user: {
-            ...MOCK_USER,
-            email,
-          },
-        })
-      );
+    const errorResponse: Record<string, string> = {};
+    if (email === "") {
+      errorResponse.email = "can't be blank";
+      return await res(ctx.status(422), ctx.json({ errors: errorResponse }));
     }
-  ),
+    if (password === "") {
+      errorResponse.password = "can't be blank";
+      return await res(ctx.status(422), ctx.json({ errors: errorResponse }));
+    }
+
+    return await res(
+      ctx.json({
+        user: {
+          ...MOCK_USER,
+          email,
+        },
+      })
+    );
+  }),
+  rest.put(baseUrl("/user"), async (req, res, ctx) => {
+    if (!hasToken(req.headers)) {
+      return await res(ctx.status(401), ctx.json(UnauthorizedErrorBody));
+    }
+
+    const {
+      user: { image, username, email, bio },
+    } = await req.json();
+
+    const updatedUser = {
+      image: image ?? MOCK_USER.image,
+      username: username ?? MOCK_USER.username,
+      email: email ?? MOCK_USER.email,
+      bio: bio ?? MOCK_USER.bio,
+      token: MOCK_USER.token,
+    };
+
+    return await res(ctx.json({ user: updatedUser }));
+  }),
 ];
 
 export default handlers;
+
+function hasToken(headers: Headers): boolean {
+  const prefix = "Token ";
+  const authorization = headers.get("Authorization");
+  if (authorization === null || !authorization.startsWith(prefix)) {
+    return false;
+  }
+
+  const token = authorization.substring(prefix.length);
+  return token !== "";
+}
